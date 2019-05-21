e95e879a6c6e8a678c4afe2930d476d511953c97
17010180 <17010180@myrp.edu.sg>
2019-05-21 13:17:44 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demodatabasecrud/Note.java
@@ -0,0 +1,38 @@
+package sg.edu.rp.c346.demodatabasecrud;
+
+import java.io.Serializable;
+
+public class Note implements Serializable {
+
+    private int id;
+    private String noteContent;
+
+    public Note(int id, String noteContent) {
+        this.id = id;
+
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+    @Override
+    public String toString() {
+        return "ID:" + id + ", " + noteContent;
+    }
+
+}
+
+
